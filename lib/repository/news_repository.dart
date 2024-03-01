d55cd69ee3644ba780add58fb80d385780528df0
AnishHazra <anishhazra108@gmail.com>
2024-03-01 13:27:49 +0530

    top headlines done

---
+++ lib/repository/news_repository.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:news_app/models/news_channel_headlines_model.dart';
+
+class NewsRepository {
+  Future<NewsChannelsHeadlinesModel> fetchNewsChannelHealinesApi() async {
+    String url =
+        "https://newsapi.org/v2/top-headlines?country=in&apiKey=88a435fa7b5947ae9d6ea2d489b70005";
+
+    final response = await http.get(Uri.parse(url));
+    if (kDebugMode) {
+      print(response.body);
+    }
+    if (response.statusCode == 200) {
+      final body = jsonDecode(response.body);
+      return NewsChannelsHeadlinesModel.fromJson(body);
+    }
+    throw Exception('Error');
+  }
+}
